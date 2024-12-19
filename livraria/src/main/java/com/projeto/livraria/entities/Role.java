6f67bf17e23787b94419199d115169f31b74a023
kaique oliveira <kaique20003@gmail.com>
2024-12-19 09:24:16 -0300

    docker

---
+++ livraria/src/main/java/com/projeto/livraria/entities/Role.java
@@ -0,0 +1,42 @@
+package com.projeto.livraria.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String name;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public enum Values{
+
+        BASIC(2L),
+        ADMIN(1L);
+
+        long roleID;
+
+        Values(long roleID) {
+            this.roleID = roleID;
+        }
+    }
+}
